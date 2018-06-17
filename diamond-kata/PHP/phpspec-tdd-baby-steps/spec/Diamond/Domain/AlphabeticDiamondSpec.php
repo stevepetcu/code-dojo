07c5d702254604b2fbd4a639979146b23fa545a9
Stefan Petcu <petcu.stephan@gmail.com>
2018-06-17 07:12:30 +0100

    Add test for edge case.

--- diamond-kata/PHP/phpspec-tdd-baby-steps/spec/Diamond/Domain/AlphabeticDiamondSpec.php
+++ diamond-kata/PHP/phpspec-tdd-baby-steps/spec/Diamond/Domain/AlphabeticDiamondSpec.php
@@ -26,20 +26,29 @@ class AlphabeticDiamondSpec extends ObjectBehavior
         $alphabet->indexOf('A')->willReturn(0);
         $alphabet->indexOf('B')->willReturn(1);
         $alphabet->indexOf('C')->willReturn(2);
-
-        $this->beConstructedWith($alphabet, 'C');
     }
 
-    function it_is_instantiable()
+    function it_is_instantiable($alphabet)
     {
+        $this->beConstructedWith($alphabet, 'C');
+
         $this->shouldHaveType(AlphabeticDiamond::class);
     }
 
-    function it_can_output_a_diamond()
+    function it_can_output_a_diamond($alphabet)
     {
+        $this->beConstructedWith($alphabet, 'C');
+
         $expectedOutput = "  A\n B B\nC   C\n B B\n  A";
 
         $this->__toString()->shouldBe($expectedOutput);
+    }
+
+    function it_correctly_handles_edge_cases_pass_in_first_character_of_alphabet($alphabet)
+    {
+        $this->beConstructedWith($alphabet, 'A');
+
+        $this->__toString()->shouldBe('A');
     }
 
     function it_will_throw_invalid_argument_exception_if_2nd_constructor_argument_is_not_an_english_letter($alphabet)
