f241a246d9e7f81a3d3ca05d687a20415e28d775
Stefan Petcu <petcu.stephan@gmail.com>
2018-06-17 13:03:03 +0100

    Add license.

--- diamond-kata/Java/junit-tdd/src/main/java/codedojo/kata/junittdd/diamond/Diamond.java
+++ diamond-kata/Java/junit-tdd/src/main/java/codedojo/kata/junittdd/diamond/Diamond.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the DiamondKata application.
+ *
+ * © Stefan Petcu <hello@stefanpetcu.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 package main.java.codedojo.kata.junittdd.diamond;
 
 import java.util.Scanner;
